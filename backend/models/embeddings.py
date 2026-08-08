b449bcbc0599619abfff50869bcdc13668b1a824
umasri7 <umasri2705@gmail.com>
2026-08-08 11:34:16 +0530

    Fixed embedding shape issue

--- backend/models/embeddings.py
+++ backend/models/embeddings.py
@@ -1,6 +1,2 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-vectorizer = TfidfVectorizer()
-
 def create_embedding(text):
-    return vectorizer.fit_transform([text]).toarray()[0]
+    return get_model().encode([text])[0]
